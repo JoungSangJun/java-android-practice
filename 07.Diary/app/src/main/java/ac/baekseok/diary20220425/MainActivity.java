dedd4a4f9a37f2903a45f27ceb9e3adde6c94ef5
JoungSangJun <gogumi4502@nate.com>
2022-10-15 15:22:50 +0900

    7weeks

--- 07.Diary/app/src/main/java/ac/baekseok/diary20220425/MainActivity.java
+++ 07.Diary/app/src/main/java/ac/baekseok/diary20220425/MainActivity.java
@@ -2,11 +2,17 @@ package ac.baekseok.diary20220425;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -34,9 +40,49 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDateChanged(DatePicker datePicker, int i, int i1, int i2) {
                 //i년도, i1월, i2 일 값
-                fileName = Integer.toString(i)+"_"+Integer.toString(i1+1)+"_"+Integer.toString(i2)+".txt";
-               //readDiary(fileName);
+                fileName = Integer.toString(i) + "_" + Integer.toString(i1 + 1) + "_" + Integer.toString(i2) + ".txt";
+                //선택날 이미 화일이 있는지 보고 있으면 보여줌
+                String str = readDiary(fileName);
+                edtDiary.setText(str); //읽어온 선택날짜의 일기내용으로 내용 설정
+                btnWrite.setEnabled(true);//버튼 활성화 처리
+            }
+        });
+
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override //쓰기버튼
+            public void onClick(View view) {
+                try {
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = edtDiary.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(),fileName+"저장됨",Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+
+
+                }
             }
         });
     }//onCreate
+
+    String readDiary(String fName) {
+        String diaryStr = null;
+
+        FileInputStream inFs;
+        try { //화일 정상적으로 열렸을 때
+            inFs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);//fName의 이름으로 화일 읽기 txt에 저장
+            inFs.close();
+            diaryStr = (new String(txt)).trim(); //앞 뒤 여백제거 trim()
+            btnWrite.setText("수정하기");
+        } catch (IOException e) { //안 열렸을 때
+            edtDiary.setText("일기가 없습니다.");
+            btnWrite.setText("새로저장");
+        }
+
+
+        return diaryStr;
+    }
+
 }//MainActivity
